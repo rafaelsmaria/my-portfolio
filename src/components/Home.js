0aca4f00d71a6be9bbd6069d8b31234db835287e
Rafael Maria <rafaelsmaria01@gmail.com>
2021-07-23 20:11:37 -0300

    alter home background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import background from '../assets/background1.jpg';
+import background from '../assets/background.png';
 
 export default function Home() {
     return (
         <main>
             <img src={background} alt='Background' className='absolute object-cover w-full h-full ' />
-            <section className='relative ' >
-                <h1 className='text-2xl text-white pt-64 px-5' >
-                Welcome to my portfolio!
-                </h1>
-            </section>
         </main>
     )
 }
